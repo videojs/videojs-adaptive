8f6edf18d5a833e27d8a7f28b4e656c39a690455
heff <git@heff.me>
2014-11-17 18:07:26 -0800

    Added jquery-plugin style UMD for videojs

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-module.exports = function(window){
-  var adaptive = {};
-  
-  adaptive.SourceBufferQueue = require('./src/source-buffer-queue.js');
-
-  return adaptive;
-};
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['videojs'], factory);
+    } else if (typeof exports === 'object') {
+        // Node/CommonJS
+        factory(require('videojs'));
+    } else {
+        // Browser globals
+        factory(videojs);
+    }
+}(function (videojs) {
+    // Register the source handler
+    videojs.Html5.registerSourceHandler({}, 0);
+}));
